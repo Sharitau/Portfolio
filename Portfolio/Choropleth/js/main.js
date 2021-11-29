d0a8d42cfe2901cab3b48056eb88e2e611121fdf
Sharitau <34288355+Sharitau@users.noreply.github.com>
2021-11-28 23:08:27 -0500

    Add files via upload

---
+++ Portfolio/Choropleth/js/main.js
@@ -0,0 +1,350 @@
+//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+(function(){
+
+  //pseudo-global variables
+var attrArray = ["oPos","aPos","bPos","abPos","oNeg","aNeg","bNeg","abNeg"];//list of attributes
+  var expressed = attrArray[0];
+
+//begin script when window loads
+window.onload = setMap();
+
+//Example 1.4 line 1...set up choropleth map
+function setMap(){
+
+  //map frame dimensions
+  var width = window.innerWidth * 0.5,
+       height = 460;
+
+
+   //create new svg container for the map
+   var map = d3.select("body")
+       .append("svg")
+       .attr("class", "map")
+       .attr("width", width)
+       .attr("height", height);
+
+   //create Albers equal area conic projection centered on France
+   var projection = d3.geoMercator()
+   .center([20, 13 ])
+       .scale(240)
+       .rotate([350,0.01]);
+
+  //Setting path
+    var path = d3.geoPath()
+        .projection(projection);
+  //Example 1.4 line 3...use d3.queue to parallelize asynchronous data loading
+    d3.queue()
+        .defer(d3.csv, "data/BloodType.csv") //load attributes from csv
+        .defer(d3.json, "data/Countries2.topojson") //load background spatial data
+        .defer(d3.json, "data/Countries.topojson")
+        .await(callback);
+
+
+      function callback(error, csvData, europe, france){
+          setGraticule(map,path);
+
+        //translate europe TopoJSON
+         var europeCountries = topojson.feature(europe, europe.objects.ne_10m_admin_0_countries),
+             franceRegions = topojson.feature(france, france.objects.ne_10m_admin_0_countries).features;
+
+         //add Europe countries to map
+         var countries = map.append("path")
+             .datum(europeCountries)
+             .attr("class", "countries")
+             .attr("d", path);
+
+        //joins CSV data to GEOJSON
+        franceRegions = joinData(franceRegions, csvData);
+
+        //create the color scale
+        var colorScale = makeColorScale(csvData);
+
+        //Add Enumeration units to the setMap
+        setEnumerationUnits(franceRegions, map, path, colorScale);
+        setChart(csvData, colorScale);
+        createDropdown();
+        changeAttribute();
+
+
+      };
+
+      //function to create a dropdown menu for attribute selection
+      function createDropdown(csvData){
+    //add select element
+    var dropdown = d3.select("body")
+        .append("select")
+        .attr("class", "dropdown")
+        .on("change", function(){
+            changeAttribute(this.value, csvData)
+        });
+
+
+      var titleOption = dropdown.append("option")
+        .attr("class", "titleOption")
+        .attr("disabled", "true")
+        .text("Select Attribute");
+
+      //create each option element within the dropdown
+      var attrOptions = dropdown.selectAll("attrOptions")
+        .data(attrArray)
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d })
+        .text(function(d) { return d });
+};
+
+//dropdown change listener handler
+//dropdown change listener handler
+function changeAttribute(attribute, csvData){
+    //change the expressed attribute
+    expressed = attribute;
+
+    //recreate the color scale
+    var colorScale = makeColorScale(csvData);
+
+    //recolor enumeration units
+    var regions = d3.selectAll(".regions") //select every region
+															.style("fill", function(d) { //color enumeration units
+																return choropleth(d.properties, colorScale); //->
+															})
+
+};
+
+
+
+  };//setmap ender
+
+
+
+
+
+
+
+    //function to create coordinated bar chart
+    function setChart(csvData, colorScale){
+        //chart frame dimensions
+        var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 460;
+        leftPadding = 25,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+        //create a second svg element to hold the bar chart
+        var chart = d3.select("body")
+            .append("svg")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("class", "chart");
+
+
+            //create a rectangle for chart background fill
+        var chartBackground = chart.append("rect")
+          .attr("class", "chartBackground")
+          .attr("width", chartInnerWidth)
+          .attr("height", chartInnerHeight)
+          .attr("transform", translate);
+
+            //create a scale to size bars proportionally to frame
+          var yScale = d3.scaleLinear()
+              .range([chartHeight, 0])
+              .domain([0, 105]);
+
+            //set bars for each province
+          var bars = chart.selectAll(".bar")
+          .data(csvData)
+          .enter()
+          .append("rect")
+          .sort(function(a, b){
+              return b[expressed]-a[expressed]
+          })
+          .attr("class", function(d){
+              return "bar " + d.ADMIN;
+          })
+          .attr("width", chartInnerWidth / csvData.length - 1)
+          .attr("x", function(d, i){
+              return i * (chartInnerWidth / csvData.length) + leftPadding;
+          })
+          .attr("height", function(d, i){
+            return 463 - yScale(parseFloat(d[expressed]));
+          })
+          .attr("y", function(d, i){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
+          })
+          .style("fill", function(d){
+            return choropleth(d, colorScale);
+          });
+
+          var chartTitle = chart.append("text")
+          .attr("x", 40)
+          .attr("y", 40)
+          .attr("class", "chartTitle")
+          .text("Percentage of Blood Type " + expressed[3] + " in each Country");
+
+      //create vertical axis generator
+      var yAxis = d3.axisLeft()
+          .scale(yScale)
+
+
+      //place axis
+      var axis = chart.append("g")
+          .attr("class", "axis")
+          .attr("transform", translate)
+          .call(yAxis);
+
+      //create frame for chart border
+      var chartFrame = chart.append("rect")
+          .attr("class", "chartFrame")
+          .attr("width", chartInnerWidth)
+          .attr("height", chartInnerHeight)
+          .attr("transform", translate);
+  };
+
+
+
+
+
+
+
+
+  //function to create color scale generator
+  //function to create color scale generator
+  function makeColorScale(data){
+
+
+      var colorClasses = [
+          "#D4B9DA",
+          "#C994C7",
+          "#DF65B0",
+          "#DD1C77",
+          "#980043"
+      ];
+
+      //create color scale generator
+      var colorScale = d3.scaleQuantile()
+          .range(colorClasses)
+    try{
+//THIS BLOCK WON"T READ LENGTH
+      var domainArray = [];
+      for (var i=0; i<data.length; i++){
+          var val = parseFloat(data[i][expressed]);
+          domainArray.push(val);
+      };
+
+      //assign array of expressed values as scale domain
+      colorScale.domain(domainArray);
+
+      return colorScale;
+  }
+  catch(err) {
+    console.log('hi')
+
+  }
+
+};
+//function to test for data value and return color
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props[expressed]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == 'number' && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
+    };
+};
+
+    function setGraticule(map,path){
+              //Example 2.5 line 3...create graticule generator
+                   var graticule = d3.geoGraticule()
+                       .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+
+
+                       //create graticule background
+              var gratBackground = map.append("path")
+                  .datum(graticule.outline()) //bind graticule background
+                  .attr("class", "gratBackground") //assign class for styling
+                  .attr("d", path) //project graticule
+
+                   //create graticule lines
+                   var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+                       .data(graticule.lines()) //bind graticule lines to each element to be created
+                       .enter() //create an element for each datum
+                       .append("path") //append each element to the svg as a path element
+                       .attr("class", "gratLines") //assign class for styling
+                       .attr("d", path); //project graticule lines
+    };
+
+    function joinData(franceRegions, csvData){
+      //loop through csv to assign each set of csv attribute values to geojson region
+for (var i=0; i<csvData.length; i++){
+    var csvRegion = csvData[i]; //the current region
+    var csvKey = csvRegion.ADMIN; //the CSV primary key
+
+    //loop through geojson regions to find correct region
+    for (var a=0; a<franceRegions.length; a++){
+
+        var geojsonProps = franceRegions[a].properties; //the current region geojson properties
+        var geojsonKey = geojsonProps.ADMIN; //the geojson primary key
+
+        //where primary keys match, transfer csv data to geojson properties object
+        if (geojsonKey == csvKey){
+
+            //assign all attributes and values
+            attrArray.forEach(function(attr){
+                var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                geojsonProps[attr] = val; //assign attribute and value to geojson properties
+            });
+        };
+    };
+};
+return franceRegions;
+};
+
+function setEnumerationUnits(franceRegions, map, path, colorScale){
+  //add France regions to map
+  var regions = map.selectAll(".regions")
+      .data(franceRegions)
+      .enter()
+      .append("path")
+      .attr("class", function(d){
+          return "regions " + d.properties.ADMIN;
+      })
+      .attr("d", path)
+      .style("fill", function(d){
+           return colorScale(d.properties[expressed]);
+       })
+       .style("fill", function(d){
+           return choropleth(d.properties, colorScale);
+       });
+
+};
+
+//function to test for data value and return color
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props[expressed]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == 'number' && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
+    };
+};
+
+
+
+
+             //variables for data join
+
+
+   //loop through csv to assign each set of csv attribute values to geojson region
+ //load choropleth spatial data
+
+
+
+
+})();
